9678b80fcd60b23998de9160f8aac0e88a2b531b
kubek155 <jakubkox85@gmail.com>
2026-03-18 18:32:28 +0100

    kon dodany

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
 loadGame();
+
+// 🔥 STARTOWY KOŃ
+if (playerHorses.length === 0) {
+  playerHorses.push({
+    name: "Startowy koń",
+    stats: {
+      speed: 50,
+      strength: 50,
+      stamina: 50
+    }
+  });
+
+  saveGame();
+}
+
 renderLocations();
 setInterval(render, 1000);
