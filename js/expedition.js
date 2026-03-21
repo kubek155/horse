331632099e9c9dd03fb5b943dc8238fbb85b56de
kubek155 <jakubkox85@gmail.com>
2026-03-21 14:28:39 +0100

    moze zadziala pozdro hmtl sie wyjebal

--- js/expedition.js
+++ js/expedition.js
@@ -1,59 +1,67 @@
-const EXPEDITION_TIME=60000; // test
+const EXPEDITION_TIME=60000;
 const DAILY_LIMIT=4;
 
-const LOCATIONS=[{name:"Las"},{name:"Góry"}];
+const LOCATIONS=[
+{name:"Las"},
+{name:"Pustynia"},
+{name:"Góry"},
+{name:"Tundra"}
+];
 
 function getTodayKey(){
-  let d=new Date();
-  return d.getFullYear()+"-"+d.getMonth()+"-"+d.getDate();
+let d=new Date();
+return d.getFullYear()+"-"+d.getMonth()+"-"+d.getDate();
 }
 
 function getDailyCount(){
-  return parseInt(localStorage.getItem("d_"+getTodayKey()))||0;
+return parseInt(localStorage.getItem("d_"+getTodayKey()))||0;
 }
 
 function addDaily(){
-  localStorage.setItem("d_"+getTodayKey(),getDailyCount()+1);
+localStorage.setItem("d_"+getTodayKey(),getDailyCount()+1);
 }
 
 function renderLocations(){
-  locations.innerHTML="";
-  LOCATIONS.forEach((l,i)=>{
-    let b=document.createElement("button");
-    b.innerText=l.name;
-    b.onclick=()=>startExpedition(i);
-    locations.appendChild(b);
-  });
+locations.innerHTML="";
+LOCATIONS.forEach((l,i)=>{
+let b=document.createElement("button");
+b.innerText=l.name;
+b.onclick=()=>startExpedition(i);
+locations.appendChild(b);
+});
 }
 
 function startExpedition(i){
-  if(getDailyCount()>=DAILY_LIMIT){log("limit");return;}
+if(getDailyCount()>=DAILY_LIMIT){log("limit");return;}
 
-  expeditions.push({
-    end:Date.now()+EXPEDITION_TIME,
-    location:LOCATIONS[i],
-    done:false
-  });
+expeditions.push({
+end:Date.now()+EXPEDITION_TIME,
+location:LOCATIONS[i],
+done:false
+});
 
-  addDaily();
-  saveGame();
+addDaily();
+saveGame();
 }
 
 function finishExpedition(e){
-  let r=Math.random()*100;
+let r=Math.random()*100;
 
-  if(r<20){
-    playerHorses.push(generateHorse());
-    log("Nowy koń!");
-  }else if(r<40){
-    addItem("Eliksir");
-    log("Item!");
-  }else{
-    log("Nic");
-  }
+if(r<20){
+playerHorses.push(generateHorse());
+log("Nowy koń!");
+}
+else if(r<40){
+addItem("Eliksir");
+log("Item!");
+}
+else log("Nic");
 
-  e.done=true;
-  saveGame();
+e.done=true;
+saveGame();
 }
 
-function log(t){logDiv.innerText=t;}
+function log(t){
+  let logDiv = document.getElementById("log");
+  if(logDiv) logDiv.innerText = t;
+}
